131a87d048618a73249247f6f744ae9406401a44
azurshinano <azurshinano@outlook.com>
2024-05-21 07:18:15 -0700

    2024_05_21 commit_03

--- orm/owned_appreciate.go
+++ orm/owned_appreciate.go
@@ -1,7 +1,6 @@
 package orm
 
 type OwnedAppreciate struct {
-	ID             uint32 `gorm:"primary_key"`
 	CommanderID    uint32 `gorm:"not_null;primaryKey"`
 	AppreciateID   uint32 `gorm:"not_null;primaryKey"`
 	AppreciateType uint32 `gorm:"not_null;primaryKey"`
